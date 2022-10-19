2c3145c36692167336d88d1393aae86baac37dc6
Rolando <rolando@pop-os.localdomain>
2022-10-19 14:19:54 -0600

    Primer Commit

---
+++ src/components/global/NabVar.vue
@@ -0,0 +1,125 @@
+<template>
+  <div>
+    <nav>
+      <v-toolbar
+        src="@/assets/img/nabvarfondo.jpeg"
+        app
+        v-if="!$vuetify.breakpoint.sm && !$vuetify.breakpoint.xs"
+      >
+        <v-toolbar-title>Portal de Transparencia</v-toolbar-title>
+        <v-btn color="#696969" dark text>
+          <v-icon size="20">mdi-home</v-icon>
+          Inicio
+        </v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="#696969" text dark v-bind="attrs" v-on="on">
+              Información Oficiosa <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title>Solicitud de Información</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>Informacion Oficiosa</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+        <v-btn text color="#696969">
+          <v-icon>mdi-account-box</v-icon> Contactenos
+        </v-btn>
+        <v-spacer></v-spacer>
+
+        <v-text-field dense placeholder="Buscar..." hide-details></v-text-field>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-toolbar src="@/assets/img/nabvarfondo.jpeg" app v-else>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+
+        <v-text-field dense placeholder="Buscar..." hide-details></v-text-field>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </nav>
+    <v-navigation-drawer
+      src="@/assets/img/nabvarfondo.jpeg"
+      app
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-group :value="true" prepend-icon="mdi-account-circle">
+          <template v-slot:activator>
+            <v-list-item-title>Informacion Oficiosa</v-list-item-title>
+          </template>
+          <v-list-item link class="mx-4">
+            <v-list-item-icon>
+              <v-icon>mdi-circle-box-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Solicitar Informacion</v-list-item-title>
+          </v-list-item>
+          <v-list-item link class="mx-4">
+            <v-list-item-icon>
+              <v-icon>mdi-circle-box-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Información Oficiosa</v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-box</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Contactenos</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NabVar",
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: "Inicio", icon: "mdi-home" },
+        { title: "Información Oficiosa", icon: "mdi-folder-information" },
+        { title: "Users", icon: "mdi-account-group-outline" },
+      ],
+      mini: true,
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,154 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-card elevation="2" outlined shaped>
+      <v-card-title>
+        <h1>Ejemplo Titulo</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col cols="6">
+            <v-card elevation="2" outlined>
+              <v-card-title>
+                <h3>Como solicitar Informacion</h3>
+                <br />
+              </v-card-title>
+              <v-divider class="mx-4"></v-divider>
+              <v-card-text>
+                <ol>
+                  <li>Descargar el formulario</li>
+                  <li>Llenarlo con todos los datos.</li>
+                  <li>
+                    <v-btn
+                      @click="revealEmail = !revealEmail"
+                      text
+                      color="success"
+                    >
+                      Enviar Correo
+                    </v-btn>
+                    ó
+                    <v-btn
+                      @click="revealAddress = !revealAddress"
+                      text
+                      color="success"
+                    >
+                      Entregar Solicitud
+                    </v-btn>
+                  </li>
+                  <!-- <li>
+                    Enviar junto con la solicitud copia del Documento de
+                    Identidad (DUI).
+                  </li> -->
+                </ol>
+              </v-card-text>
+              <v-expand-transition>
+                <v-card
+                  v-if="revealEmail"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%; width: 95%"
+                >
+                  <v-card-text class="pb-0">
+                    <p class="text-h5 text--primary">Correo Institucional</p>
+                    <ul>
+                      <li>transparenciainstitucional@fgr.gob.sv</li>
+                    </ul>
+                  </v-card-text>
+                  <v-card-actions class="pt-10">
+                    <v-btn text color="red" @click="revealEmail = false">
+                      Cerrar
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+                <v-card
+                  v-if="revealAddress"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%; width: 95%"
+                >
+                  <v-card-text class="pb-0">
+                    <p class="text-h5 text--primary">Dirección</p>
+                    <ul>
+                      <li>
+                        Entregar la solicitud personalmente en la siguiente
+                        dirección: Boulevard Orden de Malta, Avenida El Boquerón
+                        # 4-A, Edificio Zafiro, Santa Elena, Antiguo Cuscatlán,
+                        La Libertad
+                      </li>
+                    </ul>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn text color="red" @click="revealAddress = false">
+                      Cerrar
+                    </v-btn>
+
+                    <a
+                      href="http://portaldetransparencia.fgr.gob.sv/documentos/Formulario-de-Solicitud-de-Informacion.pdf"
+                      target="_blank"
+                      class="v-btn v-btn--text"
+                    >
+                      Descargar PDF</a
+                    >
+                  </v-card-actions>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+          </v-col>
+          <v-col cols="6">
+            <v-card elevation="2" outlined>
+              <v-card-title>
+                <h3>Como solicitar Informacion</h3>
+                <br />
+              </v-card-title>
+              <v-divider class="mx-4"></v-divider>
+              <v-card-text>
+                <ol>
+                  <li>Descargar el formulario</li>
+                  <li>Llenarlo con todos los datos.</li>
+                  <li>
+                    <v-btn
+                      @click="revealEmail = !revealEmail"
+                      text
+                      color="success"
+                    >
+                      Enviar Correo
+                    </v-btn>
+                    ó
+                    <v-btn
+                      @click="revealAddress = !revealAddress"
+                      text
+                      color="success"
+                    >
+                      Entregar Solicitud
+                    </v-btn>
+                  </li>
+                  <!-- <li>
+                    Enviar junto con la solicitud copia del Documento de
+                    Identidad (DUI).
+                  </li> -->
+                </ol>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+
+  data: () => ({
+    revealEmail: false,
+    revealAddress: false,
+  }),
 };
 </script>
+
+<style scoped>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
